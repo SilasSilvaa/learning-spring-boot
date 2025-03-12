c5cea0f3104dbdf59e63bb784d9b0f89df8e6242
SilasSilvaa <silas-oli@hotmail.com>
2025-03-12 01:24:40 -0300

    feat: add Math controller

---
+++ src/main/java/com/ssilvadev/annotationsAndExceptions/controllers/MathController.java
@@ -0,0 +1,39 @@
+package com.ssilvadev.annotationsAndExceptions.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo) throws Exception {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedOperationException("Please set a numeric value");
+        }
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null || strNumber.isEmpty()) {
+            return false;
+        }
+
+        String number = strNumber.replace(",", ".");
+
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+
+    private Double convertToDouble(String strNumber) throws IllegalArgumentException {
+        if (strNumber == null || strNumber.isEmpty()) {
+            throw new UnsupportedOperationException("Please set a numeric value");
+        }
+
+        return Double.parseDouble(strNumber);
+    }
+}
