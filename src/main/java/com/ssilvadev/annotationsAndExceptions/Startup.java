30aea346ff0cdc8a6e1b24b7005f647426a6f6e7
SilasSilvaa <silas-oli@hotmail.com>
2025-03-11 22:26:44 -0300

    --

--- src/main/java/com/ssilvadev/annotationsAndExceptions/Startup.java
+++ src/main/java/com/ssilvadev/annotationsAndExceptions/Startup.java
@@ -1,4 +1,4 @@
-package com.ssilvadev.firststep;
+package com.ssilvadev.annotationsAndExceptions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
