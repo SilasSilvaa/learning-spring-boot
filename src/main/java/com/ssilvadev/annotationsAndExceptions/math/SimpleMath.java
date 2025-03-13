a39ef82de327f22c1fa0d8042741b4edcc1bf87e
SilasSilvaa <silas-oli@hotmail.com>
2025-03-13 09:22:24 -0300

    feat: add class for resturn basic math operations

---
+++ src/main/java/com/ssilvadev/annotationsAndExceptions/math/SimpleMath.java
@@ -0,0 +1,38 @@
+package com.ssilvadev.annotationsAndExceptions.math;
+
+public class SimpleMath {
+
+    public Double sum(Double numberOne, Double numberTwo) {
+
+        return numberOne + numberTwo;
+    }
+
+    public Double sub(Double numberOne, Double numberTwo) {
+
+        return numberOne - numberTwo;
+    }
+
+    public Double mult(Double numberOne, Double numberTwo) {
+
+        return numberOne * numberTwo;
+    }
+
+    public Double div(Double numberOne, Double numberTwo) throws Exception {
+
+        if (numberOne <= 0 || numberTwo <= 0) {
+            throw new UnsupportedOperationException("Please set a numeric greater than zero");
+        }
+
+        return numberOne / numberTwo;
+    }
+
+    public Double average(Double numberOne, Double numberTwo) {
+
+        return (numberOne + numberTwo) / 2;
+    }
+
+    public Double squareroot(Double number) {
+
+        return Math.sqrt(number);
+    }
+}
