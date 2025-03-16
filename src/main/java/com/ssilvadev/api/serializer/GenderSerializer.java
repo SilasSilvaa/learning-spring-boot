17684b8cfd7f306bb7fa69e11c8dedfa0e29c26e
SilasSilvaa <silas-oli@hotmail.com>
2025-03-16 13:35:44 -0300

    feat: create json format for property gender

---
+++ src/main/java/com/ssilvadev/api/serializer/GenderSerializer.java
@@ -0,0 +1,17 @@
+package com.ssilvadev.api.serializer;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+public class GenderSerializer extends JsonSerializer<String> {
+
+    @Override
+    public void serialize(String gender, JsonGenerator gen, SerializerProvider arg2) throws IOException {
+        String formatedGender = "Male".equals(gender) ? "M" : "F";
+        gen.writeString(formatedGender);
+    }
+
+}
