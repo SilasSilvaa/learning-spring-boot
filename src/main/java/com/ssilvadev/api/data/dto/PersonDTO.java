937fe802612823e174f0d476066f4ea1f45cc134
SilasSilvaa <silas-oli@hotmail.com>
2025-03-16 13:37:55 -0300

    feat: using Json Properties on DTO

--- src/main/java/com/ssilvadev/api/data/dto/PersonDTO.java
+++ src/main/java/com/ssilvadev/api/data/dto/PersonDTO.java
@@ -1,17 +1,45 @@
 package com.ssilvadev.api.data.dto;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+// import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.ssilvadev.api.serializer.GenderSerializer;
+
+@JsonPropertyOrder({ "id", "first_name", "last_name", "gender", "address" })
+@JsonFilter("PersonFilter")
 public class PersonDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private Long id;
+
+    @JsonProperty("first_name")
     private String firstName;
+
+    @JsonProperty("last_name")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String lastName;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private String phone;
+
     private String address;
+
+    private Date birthDay;
+
+    // @JsonIgnore
+    @JsonSerialize(using = GenderSerializer.class)
     private String gender;
+
+    private String sensitiveData;
 
     public PersonDTO() {
 
@@ -41,12 +69,29 @@ public class PersonDTO implements Serializable {
         this.lastName = lastName;
     }
 
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
     public String getAddress() {
         return address;
     }
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
     }
 
     public String getGender() {
@@ -57,9 +102,18 @@ public class PersonDTO implements Serializable {
         this.gender = gender;
     }
 
+    public String getSensitiveData() {
+        return sensitiveData;
+    }
+
+    public void setSensitiveData(String sensitiveData) {
+        this.sensitiveData = sensitiveData;
+    }
+
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getFirstName(), getLastName(), getAddress(), getGender());
+        return Objects.hash(getId(), getFirstName(), getLastName(), getPhone(), getAddress(), getBirthDay(),
+                getGender(), getSensitiveData());
     }
 
     @Override
@@ -70,7 +124,10 @@ public class PersonDTO implements Serializable {
         return Objects.equals(getId(), person.getId()) &&
                 Objects.equals(getFirstName(), person.getFirstName()) &&
                 Objects.equals(getLastName(), person.getLastName()) &&
+                Objects.equals(getPhone(), person.getPhone()) &&
                 Objects.equals(getAddress(), person.getAddress()) &&
-                Objects.equals(getGender(), person.getGender());
+                Objects.equals(getBirthDay(), person.getBirthDay()) &&
+                Objects.equals(getGender(), person.getGender()) &&
+                Objects.equals(getSensitiveData(), person.getSensitiveData());
     }
 }
