1cb08f8ed15701fcd38ed9641fba27149c8d1edc
SilasSilvaa <silas-oli@hotmail.com>
2025-03-17 20:27:17 -0300

    config: add configuration for yaml conveter

---
+++ src/main/java/com/ssilvadev/api/serialization/converter/YamlJackson2HtppMessageConverter.java
@@ -0,0 +1,17 @@
+package com.ssilvadev.api.serialization.converter;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+
+public final class YamlJackson2HtppMessageConverter extends AbstractJackson2HttpMessageConverter {
+
+    protected YamlJackson2HtppMessageConverter() {
+        super(new YAMLMapper()
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL),
+                MediaType.parseMediaType("application/yaml"));
+    }
+
+}
