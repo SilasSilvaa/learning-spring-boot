1e760d02e55e27aa1588311077a2cd0d8633c101
SilasSilvaa <silas-oli@hotmail.com>
2025-03-17 23:38:53 -0300

    feat: add handler for RequiredObjectsIsNullException

--- src/main/java/com/ssilvadev/api/exception/handler/CustomEntityResponseHandler.java
+++ src/main/java/com/ssilvadev/api/exception/handler/CustomEntityResponseHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.ssilvadev.api.exception.ExceptionResponse;
+import com.ssilvadev.api.exception.RequiredObjectsIsNullException;
 import com.ssilvadev.api.exception.ResourceNotFoundException;
 import com.ssilvadev.api.exception.UnsuportedMathOperationException;
 
@@ -40,5 +41,13 @@ public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler 
                 new Date(), ex.getMessage(), request.getDescription(false));
 
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RequiredObjectsIsNullException.class)
+    public final ResponseEntity<ExceptionResponse> handleBadRequestException(Exception ex, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(
+                new Date(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
