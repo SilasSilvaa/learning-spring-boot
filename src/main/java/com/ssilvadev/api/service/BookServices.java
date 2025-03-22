89cb6be70179d6ded292c37305f01335c1e8d6c7
SilasSilvaa <silas-oli@hotmail.com>
2025-03-22 19:17:23 -0300

    refactor: uncomment code

--- src/main/java/com/ssilvadev/api/service/BookServices.java
+++ src/main/java/com/ssilvadev/api/service/BookServices.java
@@ -95,8 +95,8 @@ public class BookServices {
         dto.add(linkTo(methodOn(BookController.class).findById(dto.getId())).withSelfRel().withType("GET"));
         dto.add(linkTo(methodOn(BookController.class).findAll()).withRel("findAll").withType("GET"));
         dto.add(linkTo(methodOn(BookController.class).delete(dto.getId())).withRel("delete").withType("DELETE"));
-        // dto.add(linkTo(methodOn(BookController.class).create(dto)).withRel("create").withType("POST"));
-        // dto.add(linkTo(methodOn(BookController.class).create(dto)).withRel("update").withType("PUT"));
+        dto.add(linkTo(methodOn(BookController.class).create(dto)).withRel("create").withType("POST"));
+        dto.add(linkTo(methodOn(BookController.class).create(dto)).withRel("update").withType("PUT"));
     }
 
 }
