a70bfe2cee1d8b896ce35af1f83e30781ca527e5
SilasSilvaa <silas-oli@hotmail.com>
2025-03-23 13:48:48 -0300

    info: possible ways to use CORS in a controller

--- src/main/java/com/ssilvadev/api/controllers/PersonController.java
+++ src/main/java/com/ssilvadev/api/controllers/PersonController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.ssilvadev.api.service.PersonServices;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+// @CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api/person/v1")
 @Tag(name = "People", description = "Endpoints for mapping People")
@@ -37,6 +39,7 @@ public class PersonController implements PersonControllerDocs {
 		return service.findAll();
 	}
 
+	// @CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping(value = "/{id}", produces = {
 			MediaType.APPLICATION_JSON_VALUE,
 			MediaType.APPLICATION_XML_VALUE,
@@ -47,6 +50,7 @@ public class PersonController implements PersonControllerDocs {
 		return service.findById(id);
 	}
 
+	// @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:8181"})
 	@PostMapping(produces = {
 			MediaType.APPLICATION_JSON_VALUE,
 			MediaType.APPLICATION_XML_VALUE,
