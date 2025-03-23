56f4945f5a25de9c480a981a2d09ab8e4581042f
SilasSilvaa <silas-oli@hotmail.com>
2025-03-23 13:49:23 -0300

    feat: add new props for testing

--- src/test/java/com/ssilvadev/api/config/TestConfigs.java
+++ src/test/java/com/ssilvadev/api/config/TestConfigs.java
@@ -2,4 +2,11 @@ package com.ssilvadev.api.config;
 
 public interface TestConfigs {
     int SERVER_PORT = 8888;
+    
+    String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    String HEADER_PARAM_ORIGIN = "Origin";
+
+    String ORIGIN_1 = "http://localhost:3000";
+    String ORIGIN_2 = "http://localhost:8181";
+    String ORIGIN_3 = "http://localhost:8080";
 }
