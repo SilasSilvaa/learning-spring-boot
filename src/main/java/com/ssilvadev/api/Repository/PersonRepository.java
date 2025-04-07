c31a656e871548ff6035ff55be4f41b5516460ba
Silassilvaa <silas-oli@hotmail.com>
2025-04-07 19:51:15 -0300

    feat: craete method disableperson

--- src/main/java/com/ssilvadev/api/Repository/PersonRepository.java
+++ src/main/java/com/ssilvadev/api/Repository/PersonRepository.java
@@ -1,6 +1,9 @@
 package com.ssilvadev.api.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ssilvadev.api.model.Person;
@@ -8,4 +11,7 @@ import com.ssilvadev.api.model.Person;
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Person p SET p.enabled = false WHERE p.id =:id")
+    void disablePerson(@Param("id") Long id);
 }
