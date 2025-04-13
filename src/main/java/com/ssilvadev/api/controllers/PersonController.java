696f88e0e6b72ad339f8a31f6d9c2950839af57e
Silassilvaa <silas-oli@hotmail.com>
2025-04-12 23:03:02 -0300

    feat: using query params on find all method and implementing find by name method

--- src/main/java/com/ssilvadev/api/controllers/PersonController.java
+++ src/main/java/com/ssilvadev/api/controllers/PersonController.java
@@ -1,11 +1,14 @@
 package com.ssilvadev.api.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssilvadev.api.controllers.docs.PersonControllerDocs;
@@ -35,9 +39,32 @@ public class PersonController implements PersonControllerDocs {
 			MediaType.APPLICATION_XML_VALUE,
 			MediaType.APPLICATION_YAML_VALUE })
 	@Override
+	public ResponseEntity<PagedModel<EntityModel<PersonDTO>>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "size", defaultValue = "12") Integer size,
+			@RequestParam(value = "direction", defaultValue = "asc") String direction) {
 
-	public List<PersonDTO> findAll() {
-		return service.findAll();
+		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+
+		Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
+		return ResponseEntity.ok(service.findAll(pageable));
+	}
+
+	@GetMapping(value = "/findPeopleByName/{firstName}", produces = {
+			MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_YAML_VALUE })
+	@Override
+	public ResponseEntity<PagedModel<EntityModel<PersonDTO>>> findByName(
+			@PathVariable("firstName") String firstName,
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "size", defaultValue = "12") Integer size,
+			@RequestParam(value = "direction", defaultValue = "asc") String direction) {
+
+		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+
+		Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
+		return ResponseEntity.ok(service.findByName(firstName, pageable));
 	}
 
 	// @CrossOrigin(origins = "http://localhost:8080")
