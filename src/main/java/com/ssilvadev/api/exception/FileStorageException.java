49d200cb4eebc5e25e925c41669cc369f4e8bc96
Silassilvaa <silas-oli@hotmail.com>
2025-04-13 19:15:11 -0300

    feat: custom exception for error on file storage

---
+++ src/main/java/com/ssilvadev/api/exception/FileStorageException.java
@@ -0,0 +1,15 @@
+package com.ssilvadev.api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
