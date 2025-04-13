b2bb4316218fc251d798e9444f384237dd50bcbc
Silassilvaa <silas-oli@hotmail.com>
2025-04-12 23:00:12 -0300

    feat: add query params on findAll and create method find by name with path params and query params

--- src/main/java/com/ssilvadev/api/controllers/docs/PersonControllerDocs.java
+++ src/main/java/com/ssilvadev/api/controllers/docs/PersonControllerDocs.java
@@ -1,10 +1,11 @@
 package com.ssilvadev.api.controllers.docs;
 
-import java.util.List;
-
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ssilvadev.api.data.dto.PersonDTO;
 
@@ -26,11 +27,29 @@ public interface PersonControllerDocs {
                         @ApiResponse(description = "No Found", responseCode = "404", content = @Content),
                         @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
         })
-        List<PersonDTO> findAll();
+        ResponseEntity<PagedModel<EntityModel<PersonDTO>>> findAll(
+                        @RequestParam(value = "page", defaultValue = "0") Integer page,
+                        @RequestParam(value = "size", defaultValue = "12") Integer size,
+                        @RequestParam(value = "direction", defaultValue = "asc") String direction);
+
+        @Operation(summary = "Find People by name", description = "Find People by name", tags = "People", responses = {
+                        @ApiResponse(description = "Success", responseCode = "200", content = {
+                                        @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = PersonDTO.class)))
+                        }),
+                        @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                        @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                        @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                        @ApiResponse(description = "No Found", responseCode = "404", content = @Content),
+                        @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        })
+        ResponseEntity<PagedModel<EntityModel<PersonDTO>>> findByName(
+                        @PathVariable("firstName") String firstName,
+                        @RequestParam(value = "page", defaultValue = "0") Integer page,
+                        @RequestParam(value = "size", defaultValue = "12") Integer size,
+                        @RequestParam(value = "direction", defaultValue = "asc") String direction);
 
         @Operation(summary = "Find a Person", description = "Find a Person", tags = "People", responses = {
-                        @ApiResponse(description = "Success", responseCode = "200", 
-                                        content = @Content(schema = @Schema(implementation = PersonDTO.class))),
+                        @ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = PersonDTO.class))),
                         @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
                         @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
                         @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
