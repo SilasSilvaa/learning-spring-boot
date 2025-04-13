8f83265963c80da37ed66cce32823a6459703062
Silassilvaa <silas-oli@hotmail.com>
2025-04-12 23:01:32 -0300

    feat: using query params on method

--- src/main/java/com/ssilvadev/api/controllers/BookController.java
+++ src/main/java/com/ssilvadev/api/controllers/BookController.java
@@ -1,15 +1,20 @@
 package com.ssilvadev.api.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssilvadev.api.controllers.docs.BookControllerDocs;
 import com.ssilvadev.api.data.dto.BookDTO;
 import com.ssilvadev.api.service.BookServices;
 
-import java.util.List;
-
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -23,61 +28,68 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/api/books/v1")
 public class BookController implements BookControllerDocs {
 
-    @Autowired
-    private BookServices service;
+        @Autowired
+        private BookServices service;
 
-    @GetMapping(produces = {
-            MediaType.APPLICATION_JSON_VALUE,
-            MediaType.APPLICATION_XML_VALUE,
-            MediaType.APPLICATION_YAML_VALUE
-    })
-    @Override
-    public List<BookDTO> findAll() {
-        return service.findAll();
-    }
+        @GetMapping(produces = {
+                        MediaType.APPLICATION_JSON_VALUE,
+                        MediaType.APPLICATION_XML_VALUE,
+                        MediaType.APPLICATION_YAML_VALUE
+        })
+        @Override
+        public ResponseEntity<PagedModel<EntityModel<BookDTO>>> findAll(
+                        @RequestParam(value = "page", defaultValue = "0") Integer page,
+                        @RequestParam(value = "size", defaultValue = "12") Integer size,
+                        @RequestParam(value = "direction", defaultValue = "asc") String direction) {
 
-    @GetMapping(value = "/{id}", produces = {
-            MediaType.APPLICATION_JSON_VALUE,
-            MediaType.APPLICATION_XML_VALUE,
-            MediaType.APPLICATION_YAML_VALUE })
-    public BookDTO findById(@PathVariable Long id) {
-        return service.findById(id);
-    }
+                var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+                Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "title"));
 
-    @PostMapping(produces = {
-            MediaType.APPLICATION_JSON_VALUE,
-            MediaType.APPLICATION_XML_VALUE,
-            MediaType.APPLICATION_YAML_VALUE }, consumes = {
-                    MediaType.APPLICATION_JSON_VALUE,
-                    MediaType.APPLICATION_XML_VALUE,
-                    MediaType.APPLICATION_YAML_VALUE
-            })
-    @Override
-    public BookDTO create(@RequestBody BookDTO book) {
-        return service.create(book);
-    }
+                return ResponseEntity.ok(service.findAll(pageable));
+        }
 
-    @PutMapping(produces = {
-            MediaType.APPLICATION_JSON_VALUE,
-            MediaType.APPLICATION_XML_VALUE,
-            MediaType.APPLICATION_YAML_VALUE }, consumes = {
-                    MediaType.APPLICATION_JSON_VALUE,
-                    MediaType.APPLICATION_XML_VALUE,
-                    MediaType.APPLICATION_YAML_VALUE
-            })
-    @Override
-    public BookDTO update(@RequestBody BookDTO book) {
-        return service.update(book);
-    }
+        @GetMapping(value = "/{id}", produces = {
+                        MediaType.APPLICATION_JSON_VALUE,
+                        MediaType.APPLICATION_XML_VALUE,
+                        MediaType.APPLICATION_YAML_VALUE })
+        public BookDTO findById(@PathVariable Long id) {
+                return service.findById(id);
+        }
 
-    @DeleteMapping(value = "/{id}", produces = {
-            MediaType.APPLICATION_JSON_VALUE,
-            MediaType.APPLICATION_XML_VALUE,
-            MediaType.APPLICATION_YAML_VALUE })
-    @Override
-    public ResponseEntity<?> delete(@PathVariable Long id) {
-        service.delete(id);
+        @PostMapping(produces = {
+                        MediaType.APPLICATION_JSON_VALUE,
+                        MediaType.APPLICATION_XML_VALUE,
+                        MediaType.APPLICATION_YAML_VALUE }, consumes = {
+                                        MediaType.APPLICATION_JSON_VALUE,
+                                        MediaType.APPLICATION_XML_VALUE,
+                                        MediaType.APPLICATION_YAML_VALUE
+                        })
+        @Override
+        public BookDTO create(@RequestBody BookDTO book) {
+                return service.create(book);
+        }
 
-        return ResponseEntity.noContent().build();
-    }
+        @PutMapping(produces = {
+                        MediaType.APPLICATION_JSON_VALUE,
+                        MediaType.APPLICATION_XML_VALUE,
+                        MediaType.APPLICATION_YAML_VALUE }, consumes = {
+                                        MediaType.APPLICATION_JSON_VALUE,
+                                        MediaType.APPLICATION_XML_VALUE,
+                                        MediaType.APPLICATION_YAML_VALUE
+                        })
+        @Override
+        public BookDTO update(@RequestBody BookDTO book) {
+                return service.update(book);
+        }
+
+        @DeleteMapping(value = "/{id}", produces = {
+                        MediaType.APPLICATION_JSON_VALUE,
+                        MediaType.APPLICATION_XML_VALUE,
+                        MediaType.APPLICATION_YAML_VALUE })
+        @Override
+        public ResponseEntity<?> delete(@PathVariable Long id) {
+                service.delete(id);
+
+                return ResponseEntity.noContent().build();
+        }
 }
